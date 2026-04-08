b7d934829c0a5453f1ce100ae3f24074ee18c119
CarinaFo <carinaforster0611@gmail.com>
2026-04-08 15:00:37 +1000

    latest version of csv

--- make_reports/analyze_patient_expectation_survey.py
+++ make_reports/analyze_patient_expectation_survey.py
@@ -7,7 +7,7 @@ import re
 from matplotlib.backends.backend_pdf import PdfPages
 
 # load csv file from RedCap
-df = pd.read_csv(r"C:\Users\CarinaF\Downloads\P3990PatientExperien_DATA_LABELS_2026-01-15_1103.csv")
+df = pd.read_csv(r"C:\Users\CarinaF\Downloads\P3990PatientExperien_DATA_LABELS_2026-04-02_1325.csv")
 
 # ID 225 is ID 224 (Record ID 25), Olivia told me on the 15th of January
 
